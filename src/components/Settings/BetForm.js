fc9701cbe160d67b584fef8f3c5162df7441fba1
AVMG <45005889+AVMG20@users.noreply.github.com>
2024-05-22 07:21:36 +0000

    Added bet multipliers

--- src/components/Settings/BetForm.js
+++ src/components/Settings/BetForm.js
@@ -1,78 +1,133 @@
-import { useState, useContext } from 'react';
-import useSound from 'use-sound';
+import { useState, useContext } from "react";
+import useSound from "use-sound";
 
-import GameContext from '../../store/game-context';
-import bet from '../../assets/audio/bet.mp3';
-import cashout from '../../assets/audio/cashout.wav';
-import styles from './BetForm.module.css';
+import GameContext from "../../store/game-context";
+import bet from "../../assets/audio/bet.mp3";
+import cashout from "../../assets/audio/cashout.wav";
+import styles from "./BetForm.module.css";
 
 function BetForm() {
-    const ctx = useContext(GameContext);
-    
-    const [error, setError] = useState(false);
-    const betChangehandler = (event) => {
-        const bet = event.target.value;
-        if (bet > ctx.money) { setError(true); }
-        else { setError(false); }
+  const ctx = useContext(GameContext);
+
+  const [error, setError] = useState(false);
+  const [betAmount, setBetAmount] = useState(0.50);
+
+  const betChangehandler = (event) => {
+    const bet = parseFloat(event.target.value);
+    setBetAmount(bet);
+    if (bet > ctx.money) {
+      setError(true);
+    } else {
+      setError(false);
     }
+  };
 
-    const [playBet] = useSound(bet);
-    const [playCashout] = useSound(cashout);
-
-    const submitHandler = (event) => {
-        event.preventDefault();
-        if (!ctx.isRunning) { // Start Game
-            if (error) { return; }
-            const data = {
-                bet: +event.target.bet.value,
-                bombs: +event.target.mines.value,
-            }
-            playBet();
-            ctx.startGame(data);
-        } else { // Cashout
-            playCashout();
-            ctx.endGame(true);
-        }
+  const adjustBet = (multiplier) => {
+    let newBet = betAmount * multiplier;
+    if (newBet <= ctx.money && newBet <= 10000000) {
+        console.log(newBet);
+      setBetAmount(newBet.toFixed(2));
+      setError(false);
+    } else {
+      setError(true);
     }
+  };
 
-    const options = []
-    for (let i=1; i<25; i++) {
-        options.push(<option key={i} value={i}>{i}</option>); 
+  const [playBet] = useSound(bet);
+  const [playCashout] = useSound(cashout);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (!ctx.isRunning) {
+      // Start Game
+      if (error) {
+        return;
+      }
+      const data = {
+        bet: +event.target.bet.value,
+        bombs: +event.target.mines.value,
+      };
+      playBet();
+      ctx.startGame(data);
+    } else {
+      // Cashout
+      playCashout();
+      ctx.endGame(true);
     }
+  };
 
-    return (
-        <form className={styles.betForm} onSubmit={submitHandler}>
-            <div className={styles.input}>
-                <label htmlFor="bet">Bet Amount</label>
-                <input 
-                    name="bet" 
-                    type="number" 
-                    placeholder="0.00" 
-                    className={`${styles.inputBox} 
-                                ${styles.bet} ${error ? styles.error : undefined}`}
-                    step="0.01"
-                    min="0"
-                    max="10000000"
-                    disabled={ctx.isRunning ? "disabled" : undefined}
-                    onChange={betChangehandler}
-                />
-            </div>
-            <div className={styles.input}>
-                <label htmlFor="mines">Mines</label>
-                <select 
-                    name="mines" 
-                    defaultValue='3' 
-                    className={`${styles.inputBox} ${styles.mines}`}
-                    disabled={ctx.isRunning ? "disabled" : undefined}
-                >
-                    {options}
-                </select>
-            </div>
-            <button type="submit" className={styles.startButton}>
-                {ctx.isRunning ? 'Cashout' : 'Bet'}
-            </button>
-        </form>
+  const options = [];
+  for (let i = 1; i < 25; i++) {
+    options.push(
+      <option key={i} value={i}>
+        {i}
+      </option>,
     );
+  }
+
+  return (
+    <form className={styles.betForm} onSubmit={submitHandler}>
+      <div className={styles.input}>
+        <label htmlFor="bet">Bet Amount</label>
+        <div className={styles.buttonGroup}>
+        <button
+            type="button"
+            className={styles.button}
+            onClick={() => adjustBet(1/3)}
+          >
+            -3x
+          </button>
+          <button
+            type="button"
+            className={styles.button}
+            onClick={() => adjustBet(1/2)}
+          >
+            -2x
+          </button>
+          <input
+            name="bet"
+            type="number"
+            placeholder="0.00"
+            className={`${styles.inputBox} ${styles.bet} ${error ? styles.error : undefined}`}
+            step="0.01"
+            min="0"
+            max="10000000"
+            value={betAmount}
+            disabled={ctx.isRunning ? "disabled" : undefined}
+            onChange={betChangehandler}
+          />
+          <button
+            type="button"
+            className={styles.button}
+            onClick={() => adjustBet(2)}
+          >
+            +2x
+          </button>
+          <button
+            type="button"
+            className={styles.button}
+            onClick={() => adjustBet(3)}
+          >
+            +3x
+          </button>
+        </div>
+      </div>
+      <div className={styles.input}>
+        <label htmlFor="mines">Mines</label>
+        <select
+          name="mines"
+          defaultValue="3"
+          className={`${styles.inputBox} ${styles.mines}`}
+          disabled={ctx.isRunning ? "disabled" : undefined}
+        >
+          {options}
+        </select>
+      </div>
+      <button type="submit" className={styles.startButton}>
+        {ctx.isRunning ? "Cashout" : "Bet"}
+      </button>
+    </form>
+  );
 }
 
 export default BetForm;
